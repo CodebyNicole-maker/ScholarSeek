8ad62b3b163a6e167a1ba4fb0c61db4205d6305e
Devilwife <nicole.barger22@gmail.com>
2025-02-11 15:05:20 -0500

    updating the css and the vite

--- Develop/vite.config.ts
+++ Develop/vite.config.ts
@@ -1,25 +1,12 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  // Load environment variables
-  const env = loadEnv(mode, process.cwd());
-
-  return {
-    define: {
-      'import.meta.env': env, // Directly pass env variables (no need for JSON.stringify)
-    },
-    plugins: [react()],
-    server: {
-      watch: {
-        usePolling: true, // Fixes potential Vite file-watching issues on some systems
-      },
-      host: true, // Allows network access (useful for testing on mobile devices)
-      port: 3000, // Ensures a fixed port (change if needed)
-      strictPort: true, // Prevents Vite from picking a random port if 5173 is in use
-    },
-    optimizeDeps: {
-      exclude: ['react-router-dom'], // Ensures smooth builds when using React Router
-    },
-  };
+// https://vitejs.dev/config/
+export default defineConfig({
+  envDir: './environment', // if it says './env' its a typo you'll get 401 unauth error change it to './environment'
+  plugins: [react()],
+  server:{
+    port:3000,
+    open:true
+  }
 });
